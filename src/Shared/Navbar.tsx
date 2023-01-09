54bdf232c3298ca5aaaae9fa3d309b420621f4cc
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-01-09 17:03:51 +0600

    Footer added

--- src/Shared/Navbar.tsx
+++ src/Shared/Navbar.tsx
@@ -6,12 +6,12 @@ import {Link} from "react-router-dom"
 const Navbar = () => {
     return (
         <nav className='flex justify-between bg-slate-200 px-10 py-2 items-center'>
-            <div className='flex items-center'>
+            <div className='flex gap-3 items-center'>
                 <img className='h-16 w-16 rounded-full' src={logo} alt="" />
                 <h3 className="text-3xl text-green-600 font-extrabold">Flow Jobs</h3>
             </div>
             <div>
-                <ul className='flex gap-5 font-semibold'>
+                <ul className='flex gap-5 text-lg font-semibold'>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/">Jobs</Link></li>
                     <li><Link to="/">Friends</Link></li>
