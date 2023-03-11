8d125d4f445953ab790e2cc7189103741782e80a
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-03-12 01:11:53 +0600

    Jobs loaded on dashboard

--- src/Pages/Dashboard/Recruiter/MyPosts.tsx
+++ src/Pages/Dashboard/Recruiter/MyPosts.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import { useGetJobsQuery } from '../../../features/jobs/jobApi';
 
 const MyPosts = () => {
-    const jobPosts = [
-        {id: 1, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 2, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 3, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 4, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 5, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 6, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 7, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 8, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 9, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-        {id: 10, title: "Frontend Developer", salary: "35,000", Work: "Full Time", Location: "Remote/On site"},
-    ]
+    const {data, isLoading, isError} = useGetJobsQuery([]);
+
+    if(isLoading){
+        return <h1 className='text-3xl w-66 font-extrabold mx-auto text-center bg-blue-600 py-2 px-5 rounded text-white'>Data Loading...</h1>
+    }
+
+    if(isError){
+        return <h1 className='text-3xl w-66 font-extrabold mx-auto text-center bg-blue-600 py-2 px-5 rounded text-white'>Error while loading data.</h1>
+    }
+
     return (
         <div className='w-full'>
             <div className='flex justify-between py-1'>
@@ -30,14 +29,14 @@ const MyPosts = () => {
                 </thead>
                 <tbody>
                     {
-                        jobPosts && jobPosts.map((post, index) => {
-                            const {title, Location, Work, salary} = post;
+                        data?.jobs && data?.jobs.map((post: any, index: any) => {
+                            const {title, location, jobType, salary} = post;
                             return (
                                 <tr key={index} className='flex justify-start p-2 border hover:text-white hover:bg-blue-500'>
-                                    <td className='w-10 text-center py-2'>{index + 1}</td>
-                                    <td className='w-44 text-center py-2'>{title}</td>
-                                    <td className='w-44 text-center py-2'>{Location}</td>
-                                    <td className='w-44 text-center py-2'>{Work}</td>
+                                    <td className='w-10  py-2'>{index + 1}</td>
+                                    <td className='w-44  py-2'>{title}</td>
+                                    <td className='w-44 text-center py-2'>{location}</td>
+                                    <td className='w-44 text-center py-2'>{jobType}</td>
                                     <td className='w-44 text-center py-2'>{salary}</td>
                                     <td className='w-44 text-center'><button className='bg-red-700 px-2 py-1 rounded-lg text-white'>Delete</button></td>
                                 </tr>
