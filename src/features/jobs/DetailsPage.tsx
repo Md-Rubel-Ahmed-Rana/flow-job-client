c990eba4a6c8c62e11e376f0127eaee5402b6089
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-03-12 02:12:39 +0600

    Single job loaded

--- src/features/jobs/DetailsPage.tsx
+++ src/features/jobs/DetailsPage.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 
 const JobDetails = () => {
      const {id} = useParams();
-    
-     
+     const [details, setDetails] = useState({});
+     const {title}: any = details
+
+     useEffect(() => {
+        axios.get(`http://localhost:5000/api/jobs/${id}`)
+        .then((data) => setDetails(data.data.job))
+     }, [id])
+
     return (
         <div className='m-10'>
-            <h1 className="text-3xl">Job Details: {id}</h1>
+            <h1 className="text-3xl">Job Details: {title}</h1>
         </div>
     );
 };
