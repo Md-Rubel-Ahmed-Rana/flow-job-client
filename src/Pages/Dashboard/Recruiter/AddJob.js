ff43fb549ed8792e5db7f5ee6e55d5aae94868b4
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2023-03-12 17:38:47 +0600

    more job info added

--- src/Pages/Dashboard/Recruiter/AddJob.js
+++ src/Pages/Dashboard/Recruiter/AddJob.js
@@ -1,18 +1,22 @@
 import { useForm, useFieldArray } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useCreateJobPostMutation } from '../../../features/jobs/jobApi';
 
 const AddJob = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const { candidatesReducer: {user} } = useSelector((state) => state);
+
     const { register, handleSubmit, control, reset } = useForm();
     const {fields: skillsFields, append: addSkills, remove: removeSkill} = useFieldArray({control, name: "skills"});
     const { fields: reqFields, append: addRequirements, remove: removeRequirement } = useFieldArray({control, name: "requirements"});
     const { fields: resFields, append: addResponsibility, remove: removeResponsibility } = useFieldArray({control, name: "responsibilities"});
 
     const [postJob] = useCreateJobPostMutation();
-    const onSubmit = async(data) => {
-        const {data: {success, message}} = await postJob(data);
+    const onSubmit = async(newJob) => {
+        const { data } = await postJob(newJob);
+        const { success, message } = data || {};
         if (success){
             toast.success(message);
             reset();
@@ -23,141 +27,169 @@ const AddJob = () => {
 
     return (
         <div>
-            <h3 className="text-4xl text-center font-bold">Add New Job</h3>
-            <form onSubmit={handleSubmit(onSubmit)}  className='w-full p-10'>
-                <div className='flex justify-between gap-40'>
-                    <div className='mb-3'>
-                    <label htmlFor="title">
-                    <span className="text-xl my-2 font-semibold"> Job Title</span>
-                            <input ref={register} className='ml-2 p-2 text-xl' {...register("title", { required: true })} />
-                     </label>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="Location">
-                            <span className="text-xl my-2 font-semibold">Location</span>
-                            <input className='ml-2 p-2 text-xl' {...register("location", { required: true })} />
+        <h3 className="text-4xl text-center font-bold">Add New Job</h3>
+        <form onSubmit={handleSubmit(onSubmit)}  className='w-full p-10'>
+            <div className='flex justify-between gap-40'>
+                <div className='mb-3'>
+                <label htmlFor="title">
+                <span className="text-xl my-2 font-semibold"> Job Title</span>
+                        <input ref={register} className='ml-2 p-2 text-xl' {...register("title", { required: true })} />
                     </label>
-                    </div>
                 </div>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Location</span>
+                        <input className='ml-2 p-2 text-xl' {...register("location", { required: true })} />
+                </label>
+                </div>
+            </div>
+
+            <div className='flex justify-between gap-40'>
+                <div className='mb-3'>
+                    <span className="text-xl my-2 mr-2 font-semibold">Job Place:</span>
+                    <select {...register("jobPlace", { required: true })} className='p-2 text-xl' name="jobPlace" id="jobPlace">
+                    <option value="Remote">Remote</option>
+                    <option value="Onsite">Onsite</option>
+                    <option value="Remote/Onsite">Remote/Onsite</option>
+                </select>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Employer email</span>
+                        <input readOnly defaultValue={user?.email} className='ml-2 p-2 text-xl' {...register("employerEmail", { required: true })} />
+                </label>
+                </div>
+            </div>
+
+            <div className='flex justify-between gap-40'>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Official email</span>
+                        <input className='ml-2 p-2 text-xl' {...register("officialEmail", { required: true })} />
+                </label>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="website">
+                        <span className="text-xl my-2 font-semibold">Website</span>
+                        <input className='ml-2 p-2 text-xl' {...register("website", { required: true })} />
+                </label>
+                </div>
+            </div>
 
                 <div className='flex justify-between gap-40'>
-                    <div className='mb-3'>
-                        <span className="text-xl my-2 mr-2 font-semibold">Job type</span>
-                        <select {...register("jobType", { required: true })} className='p-2 text-xl' name="type" id="type">
-                        <option value="Remote">Remote</option>
-                        <option value="Onsite">Onsite</option>
-                        <option value="Remote/Onsite">Remote/Onsite</option>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Salary Range</span>
+                        <input className='ml-2 p-2 text-xl' {...register("salary", { required: true })} />
+                </label>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Working Day/week</span>
+                        <input className='ml-2 p-2 text-xl' {...register("workDay", { required: true })} /> 
+                </label>
+                </div>
+            </div>
+
+            <div className='flex justify-between gap-40'>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Working Time</span>
+                        <input className='ml-2 p-2 text-xl' {...register("workTime", { required: true })} /> 
+                </label>
+                </div>
+                <div className='mb-3'>
+                    <span className="text-xl my-2 mr-2 font-semibold">Job Type:</span>
+                    <select {...register("jobType", { required: true })} className='p-2 text-xl' name="jobType" id="jobType">
+                        <option selected value="Full-Time">Full-Time</option>
+                        <option value="Part-Time">Part-Time</option>
+                        <option value="Intern">Intern</option>
                     </select>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="Location">
-                            <span className="text-xl my-2 font-semibold">Employer email</span>
-                            <input className='ml-2 p-2 text-xl' {...register("employerEmail", { required: true })} />
-                    </label>
-                    </div>
                 </div>
+            </div>
 
-                <div className='flex justify-between gap-40'>
-                    <div className='mb-3'>
-                        <label htmlFor="Location">
-                            <span className="text-xl my-2 font-semibold">Official email</span>
-                            <input className='ml-2 p-2 text-xl' {...register("officialEmail", { required: true })} />
-                    </label>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="website">
-                            <span className="text-xl my-2 font-semibold">Website</span>
-                            <input className='ml-2 p-2 text-xl' {...register("website", { required: true })} />
-                    </label>
-                    </div>
-                </div>
 
-                 <div className='flex justify-between gap-40'>
-                    <div className='mb-3'>
-                        <label htmlFor="Location">
-                            <span className="text-xl my-2 font-semibold">Salary Range</span>
-                            <input className='ml-2 p-2 text-xl' {...register("salary", { required: true })} />
-                    </label>
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="Location">
-                            <span className="text-xl my-2 font-semibold">Working Day/week</span>
-                            <input className='ml-2 p-2 text-xl' {...register("workDay", { required: true })} /> 
-                    </label>
-                    </div>
+            <div className='flex justify-between gap-40'>
+                <div className='mb-3'>
+                    <label htmlFor="Location">
+                        <span className="text-xl my-2 font-semibold">Company Name</span>
+                        <input className='ml-2 p-2 text-xl' {...register("companyName", { required: true })} /> 
+                </label>
                 </div>
+                <div className='mb-3'>
+                    <span className="text-xl my-2 mr-2 font-semibold">Company Size</span>
+                    <select {...register("companySize", { required: true })} className='p-2 text-xl' name="companySize" id="companySize">
+                        <option selected value="Under 10">Under 10</option>
+                        <option value="10-50">10-50</option>
+                        <option value="50-100">50-100</option>
+                        <option value="Above 100">Above 100</option>
+                    </select>
+                </div>
+            </div>
 
-                <div className='flex justify-between gap-40'>
-                    <div className='mb-3'>
-                        <label htmlFor="Location">
-                            <span className="text-xl my-2 font-semibold">Working Time</span>
-                            <input className='ml-2 p-2 text-xl' {...register("workTime", { required: true })} /> 
-                    </label>
-                    </div>
-                    <div className='mb-3 flex gap-3'>
-                        <span className="text-xl my-2 font-semibold">About Employer</span>
-                        <textarea className="p-2 text-xl" {...register("aboutEmployer", { required: true })} placeholder="About yourself"  name="aboutEmployer" id="about" cols={20} rows={2}></textarea>
-                    </div>
-                </div>
+            <div>
+                <h2 className="text-xl font-semibold">Job overview</h2>
+                <textarea className="w-full p-4" {...register("overview", { required: true })} name="overview" id="overview" cols="30" rows="5"></textarea>
+            </div>
 
-                <div className="my-5">
-                    <div>
-                        <h2 className="text-xl my-2 font-semibold">Required Skills:</h2>
-                        {skillsFields?.map((item, index) => {
-                            return (
-                                <div key={item.key} className='flex items-center gap-3 mb-5'>
-                                    <input
-                                        className='w-full p-2 text-xl'
-                                        type='text'
-                                        {...register(`skills[${index}]`)}
-                                    />
-                                    <span onClick={() => removeSkill(index)} className="py-2 px-5 cursor-pointer hover:bg-red-700 bg-red-500 rounded-md text-white">Delete</span>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <span onClick={() => addSkills("")} className="py-2 px-5 cursor-pointer hover:bg-blue-700 bg-blue-500 rounded-md text-white">Add Skill</span>
+            <div className="my-5">
+                <div>
+                    <h2 className="text-xl my-2 font-semibold">Required Skills:</h2>
+                    {skillsFields?.map((item, index) => {
+                        return (
+                            <div key={item.key} className='flex items-center gap-3 mb-5'>
+                                <input
+                                    className='w-full p-2 text-xl'
+                                    type='text'
+                                    {...register(`skills[${index}]`)}
+                                />
+                                <span onClick={() => removeSkill(index)} className="py-2 px-5 cursor-pointer hover:bg-red-700 bg-red-500 rounded-md text-white">Delete</span>
+                            </div>
+                        );
+                    })}
                 </div>
+                <span onClick={() => addSkills("")} className="py-2 px-5 cursor-pointer hover:bg-blue-700 bg-blue-500 rounded-md text-white">Add Skill</span>
+            </div>
 
-                <div className="my-5">
-                    <div>
-                        <h2 className="text-xl my-2 font-semibold">Requirements:</h2>
-                        {reqFields?.map((item, index) => {
-                            return (
-                                <div key={item.key} className='flex items-center gap-3 mb-5'>
-                                    <input
-                                        className='w-full p-2 text-xl'
-                                        type='text'
-                                        {...register(`requirements[${index}]`)}
-                                    />
-                                    <span onClick={() => removeRequirement(index)} className="py-2 px-5 cursor-pointer hover:bg-red-700 bg-red-500 rounded-md text-white">Delete</span>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <span onClick={() => addRequirements("")} className="py-2 px-5 cursor-pointer hover:bg-blue-700 bg-blue-500 rounded-md text-white">Add Requirement</span>
+            <div className="my-5">
+                <div>
+                    <h2 className="text-xl my-2 font-semibold">Requirements:</h2>
+                    {reqFields?.map((item, index) => {
+                        return (
+                            <div key={item.key} className='flex items-center gap-3 mb-5'>
+                                <input
+                                    className='w-full p-2 text-xl'
+                                    type='text'
+                                    {...register(`requirements[${index}]`)}
+                                />
+                                <span onClick={() => removeRequirement(index)} className="py-2 px-5 cursor-pointer hover:bg-red-700 bg-red-500 rounded-md text-white">Delete</span>
+                            </div>
+                        );
+                    })}
                 </div>
-                <div className="my-5">
-                    <div>
-                        <h2 className="text-xl my-2 font-semibold">Responsibilities:</h2>
-                        {resFields?.map((item, index) => {
-                            return (
-                                <div key={item.key} className='flex items-center gap-3 mb-5'>
-                                    <input
-                                        className='w-full p-2 text-xl'
-                                        type='text'
-                                        {...register(`responsibilities[${index}]`)}
-                                    />
-                                    <span onClick={() => removeResponsibility(index)} className="py-2 px-5 cursor-pointer hover:bg-red-700 bg-red-500 rounded-md text-white">Delete</span>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <span onClick={() => addResponsibility("")} className="py-2 cursor-pointer px-5 hover:bg-blue-700 bg-blue-500 rounded-md text-white">Add Responsibility</span>
+                <span onClick={() => addRequirements("")} className="py-2 px-5 cursor-pointer hover:bg-blue-700 bg-blue-500 rounded-md text-white">Add Requirement</span>
+            </div>
+            <div className="my-5">
+                <div>
+                    <h2 className="text-xl my-2 font-semibold">Responsibilities:</h2>
+                    {resFields?.map((item, index) => {
+                        return (
+                            <div key={item.key} className='flex items-center gap-3 mb-5'>
+                                <input
+                                    className='w-full p-2 text-xl'
+                                    type='text'
+                                    {...register(`responsibilities[${index}]`)}
+                                />
+                                <span onClick={() => removeResponsibility(index)} className="py-2 px-5 cursor-pointer hover:bg-red-700 bg-red-500 rounded-md text-white">Delete</span>
+                            </div>
+                        );
+                    })}
                 </div>
-                <button className='bg-blue-600 px-10 py-3 rounded w-full text-white font-bold' type="submit">Add Job</button>
-            </form>
-        </div>
+                <span onClick={() => addResponsibility("")} className="py-2 cursor-pointer px-5 hover:bg-blue-700 bg-blue-500 rounded-md text-white">Add Responsibility</span>
+            </div>
+            <button className='bg-blue-600 px-10 py-3 rounded w-full text-white font-bold' type="submit">Add Job</button>
+        </form>
+    </div>
     );
 };
 
--- src/features/jobs/DetailsPage.tsx
+++ src/features/jobs/DetailsPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import {  useParams } from 'react-router-dom';
 import { useGetSingleJobQuery } from './jobApi';
 
 
@@ -7,13 +7,50 @@ const JobDetails = () => {
      const {id}: any = useParams();
      const {data} = useGetSingleJobQuery(id);
 
-     const {title, employerEmail, jobType, website, salary, workDay, workTime, aboutEmployer, location, skills, requirements, responsibilities } = data?.job || {}
+     console.log(data);
 
-     console.log(data?.job);
+     const {title, employerEmail, jobType, jobPlace, website, salary, workDay, workTime, location, skills, requirements, responsibilities, companySize, companyName } = data?.job || {}
 
     return (
         <div className='m-10'>
-            <h1 className="text-3xl">Job Details: {title}</h1>
+            <h1 className="text-3xl font-bold">Job Position: {title} ({jobPlace})</h1>
+            <h5 className='text-xl font-semibold'>Location: {location}.</h5>
+            <h5 className='text-xl font-semibold'>Company Name: {companyName}.</h5>
+            <h5 className='text-xl font-semibold'>Company Size: {companySize}.</h5>
+            <p>Work Day: {workDay} days/week</p>
+            <p>Job Type: {jobType}</p>
+            <p>Work Time: {workTime}/day</p>
+            <p>Salary: {salary}/month</p>
+
+            <p>Employer Email: {employerEmail}</p>
+            <p>Website: <a href={website} rel="noreferrer" target={'_blank'}  className='text-blue-700 font-extrabold' >Visit Now</a></p>
+            
+            <div>
+                <button className='bg-blue-700 py-2 px-16 mt-4 text-white rounded'>Apply</button>
+            </div>
+
+            <h5 className='text-xl mt-5 font-semibold'>Job overview</h5>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit laborum, corrupti voluptas rerum obcaecati sunt dolorem temporibus dolorum quod ipsum reprehenderit quia a ad vel quis illo saepe ea debitis cupiditate repudiandae dicta voluptates. Ab nesciunt labore, obcaecati dolores reprehenderit harum corrupti illo laborum, nulla eaque aliquid sunt quod repellat exercitationem hic cumque iusto perferendis. Quaerat numquam pariatur facilis harum illum, consequatur corporis, nobis ab consectetur odio ea beatae iste perspiciatis iure sit at! Unde, pariatur dicta! Dolores commodi impedit tempore, nihil iure, quod culpa obcaecati aliquid eligendi sint expedita eos sunt facere eaque recusandae laboriosam dolorem molestiae quis repellendus.</p>
+            <h5 className='text-2xl font-semibold mt-5'>Skills</h5>
+            <ul className=''>
+                {
+                skills && skills.map((skill: "") => <li className='list-disc ml-5'>{skill}</li>)
+                }
+            </ul>
+
+            <h5 className='text-2xl font-semibold mt-5'>Requirements</h5>
+            <ul className=''>
+                {
+                requirements && requirements.map((requirement: "") => <li className='list-disc ml-5'>{requirement}</li>)
+                }
+            </ul>
+
+            <h5 className='text-2xl font-semibold mt-5'>Responsibilities</h5>
+            <ul className=''>
+                {
+                responsibilities && responsibilities.map((responsibility: "") => <li className='list-disc ml-5'>{responsibility}</li>)
+                }
+            </ul>
         </div>
     );
 };
